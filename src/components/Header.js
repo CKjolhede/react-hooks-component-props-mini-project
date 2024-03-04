d44704b39bf7a5b37fd8db68eadeb327da3cde17
ckjolhede <ckjolhede@gmail.com>
2024-03-04 16:03:46 -0700

    header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import App from "./App";
+import React from "react";
+
+const Header = ({name}) => {
+	
+	return (
+		<div>
+			<header>
+				<h1>
+					{name}
+			</h1>
+		</header>
+		</div>);
+}
+
+export default Header;
